e7c4fe99446de4154f2f6b560ebbf5960c5ee628
Jay Bhoyar <jaybhoyar1997@gmail.com>
2020-03-28 17:37:09 +0530

    render function

--- src/index.js
+++ src/index.js
@@ -3,16 +3,48 @@ function createElement(type, props, ...children) {
 		type,
 		props: {
 			...props,
-			children
+			children: children.map(child =>
+				typeof child === "object" ? child : createTextElement(child)
+			)
 		}
 	};
 }
+function render(element, container) {
+	// TODO create dom nodes
+	const dom =
+		element.type === "TEXT_ELEMENT"
+			? document.createTextNode("")
+      : document.createElement(element.type);
+      const isProperty = key => key !== "children"
+  Object.keys(element.props)
+    .filter(isProperty)
+    .forEach(name => {
+      dom[name] = element.props[name]
+    })
+	element.props.children.forEach(child => render(child, dom));
+	container.appendChild(dom);
+}
 
-const element = React.createElement(
-	"div",
-	{ id: "foo" },
-	React.createElement("a", null, "bar"),
-	React.createElement("b")
+function createTextElement(text) {
+	return {
+		type: "TEXT_ELEMENT",
+		props: {
+			nodeValue: text,
+			children: []
+		}
+	};
+}
+const Didact = {
+	createElement,
+	render
+};
+
+/** @jsx Didact.createElement */
+const element = (
+	<div id="foo">
+		<a>bar</a>
+		<b />
+	</div>
 );
 const container = document.getElementById("root");
-ReactDOM.render(element, container);
+Didact.render(element, container);
